558131bf87c2e36eb5bf5836161bd685f75450d2
Ricardo Ramirez <ricardo.rmeza@live.com>
2021-08-03 12:13:03 -0500

    add alert on handleSubmit with setError ln25 rejection

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,42 +1,43 @@
-import React, { useRef, useState } from 'react'
-import { Form, Button, Card } from 'react-bootstrap';
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
 
 
 export default function Signup() {
 
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    const { signup } = useAuth()
-    const { error, setError } = useState('')
-    const { loading, setLoading } = useState(false)
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmRef = useRef();
+    const { signup } = useAuth();
+    const { error, setError } = useState("");
+    const { loading, setLoading } = useState(false);
 
-    async function handleSubmit(e){
-        e.preventDefaul()
-
-        if(passwordRef.current.value === passwordConfirmRef.current.value){
-            return setError('Password no match')
+    async function handleSubmit(e) {
+        e.preventDefault();
+    
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+          return setError("Passwords do not match");
         }
-
-        
-        try{
-            setError('')
-            setLoading(true)
-            await signup(emailRef.current.value, passwordRef.current.value)
-        }catch{
-            setError('failed to create')
+    
+        try {
+          setError("");
+          setLoading(true);
+          await signup(emailRef.current.value, passwordRef.current.value);
+          
+        } catch {
+          setError("Failed to create an account");
         }
-        setLoading(false)
-        
-    }
+    
+        setLoading(false);
+      }
 
     return (
         <>
         <Card>
             <Card.Body>
                 <h2 className="text-center mb-4">Sign Up</h2>
-                <Form>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit={handleSubmit}>
                     <Form.Group id="email">
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email" ref={emailRef} required />
@@ -49,7 +50,7 @@ export default function Signup() {
                         <Form.Label>Password-Confirm</Form.Label>
                         <Form.Control type="password" ref={passwordConfirmRef} required />
                     </Form.Group>
-                    <Button className="w-100" type='submint'>Sign Up</Button>
+                    <Button disabled={loading} className="w-100" type='submint'>Sign Up</Button>
                 </Form>
             </Card.Body>
         </Card>
