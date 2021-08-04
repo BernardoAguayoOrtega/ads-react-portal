17da879b15f15d0ad563c634aff9f3121557f2c6
Ricardo Ramirez <ricardo.rmeza@live.com>
2021-08-04 10:44:33 -0500

    add History and useHistory, exitoso

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,17 @@
 import React, { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function  Login() {
 
     const emailRef = useRef();
     const passwordRef = useRef();
     // const passwordConfirmRef = useRef();
-    const { signup } = useAuth();
+    const { login } = useAuth();
     const [ error, setError ] = useState("");
     const [ loading, setLoading ] = useState(false);
+    const history = useHistory();
 
     async function handleSubmit(e) {
         e.preventDefault();
@@ -19,9 +20,10 @@ export default function  Login() {
         try {
           setError("");
           setLoading(true);
-          await signup(emailRef.current.value, passwordRef.current.value);  
+          await login(emailRef.current.value, passwordRef.current.value);  
+          history.push("/")
         } catch {
-          setError("Oops! No se puede crear la cuenta");
+          setError("Oops! No se puede iniciar sesión");
         }
     
         setLoading(false);
@@ -31,7 +33,7 @@ export default function  Login() {
         <>
         <Card>
             <Card.Body>
-                <h2 className="text-center mb-4">Log in</h2>
+                <h2 className="text-center mb-4">Inicia Sesión</h2>
                 {/* { currentUser.email } */}
                 {error && <Alert variant="danger">{error}</Alert>}
                 <Form onSubmit={handleSubmit}>
@@ -45,7 +47,7 @@ export default function  Login() {
                     </Form.Group>
                     
                     <Button disabled={loading} className="w-100" type='submit'>
-                        Log in
+                        Iniciar
                     </Button>
                 </Form>
             </Card.Body>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function  Signup() {
 
@@ -11,6 +11,7 @@ export default function  Signup() {
     const { signup } = useAuth();
     const [ error, setError ] = useState("");
     const [ loading, setLoading ] = useState(false);
+    const history = useHistory();
 
     async function handleSubmit(e) {
         e.preventDefault();
@@ -24,7 +25,7 @@ export default function  Signup() {
           setLoading(true);
           await signup(emailRef.current.value, passwordRef.current.value);  
         } catch {
-          setError("Oops! No se puede crear la cuenta");
+          setError("Oops! Necesitas tener una cuenta");
         }
     
         setLoading(false);
@@ -34,7 +35,7 @@ export default function  Signup() {
         <>
         <Card>
             <Card.Body>
-                <h2 className="text-center mb-4">Inicio de sesión</h2>
+                <h2 className="text-center mb-4">Crear Cuenta</h2>
                 {/* { currentUser.email } */}
                 {error && <Alert variant="danger">{error}</Alert>}
                 <Form onSubmit={handleSubmit}>
@@ -51,13 +52,13 @@ export default function  Signup() {
                         <Form.Control type="password" ref={passwordConfirmRef} required />
                     </Form.Group>
                     <Button disabled={loading} className="w-100" type='submit'>
-                        Inicia Sesión
+                        Crear cuenta
                     </Button>
                 </Form>
             </Card.Body>
         </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account? <Link to="/login">Login</Link> 
+                ¿Ya tienes una cuenta? <Link to="/login">Inicia Sesión</Link> 
             </div>
         </>
     )
