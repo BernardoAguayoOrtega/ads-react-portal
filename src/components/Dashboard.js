209c9b983d774dcda00f52e7041fbffe402cde17
Ricardo Ramirez <ricardo.rmeza@live.com>
2021-08-04 11:05:34 -0500

    add show currentUser.email(muestra correo de perfil loeado)

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Card, Button, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function Dashboard() {
     const [error, setError ] = useState(""); 
+    const { currentUser }= useAuth();
 
     function handleLogout(){
 
@@ -13,6 +15,7 @@ export default function Dashboard() {
                 <Card.Body>
                     <h2 className="text-center mb-4">Perfil</h2>
                     {error && <Alert variant="danger">{error}</Alert>}
+                    <strong>Correo Electronico:</strong> {currentUser.email}
                 </Card.Body>               
             </Card>
                 <div className="w-100 text-center mt-2">
