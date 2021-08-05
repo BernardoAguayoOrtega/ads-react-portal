2fd02bcdb584cf4fb0eb40a846a0e4a6846cef91
Bernardo <bernardo@ubidots.com>
2021-08-04 23:01:41 -0500

    modified:   src/components/App.js 	modified:   src/index.js 	modified:   package-lock.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,55 @@
 import React from 'react';
 import Signup from './Signup';
 import { Container } from 'react-bootstrap';
-import { AuthProvider } from '../contexts/AuthContext';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Login from './Login';
 import PrivateRoute from './PrivateRoute';
 import ForgotPassword from './ForgotPassword';
 import UpdateProfile from './UpdateProfile';
-
+import { Card } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
 
 function App() {
-  return (
-      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight:"100vh" }}>
-      <div className="w-50" styles={{ maxWidth: '400px' }}>
-          <Router>
-            <AuthProvider>
-              <Switch>
-                <PrivateRoute exact path="/" component={Dashboard}/>
-                <PrivateRoute path="/update-profile" component={UpdateProfile}/>
-                <Route path="/signup" component={Signup} />
-                <Route path="/login" component={Login} />
-                <Route path="/forgot-password" component={ForgotPassword} />
-              </Switch>
-            </AuthProvider>
-          </Router>
-        
-      </div>
-    </Container>
-      
-  ) 
+	const { currentUser } = useAuth();
+	return (
+		<Container
+			className='d-flex align-items-center justify-content-center'
+			style={{ minHeight: '100vh' }}>
+			{currentUser && (
+				<Card>
+					<Card.Body>
+						<ul>
+							<li>
+								<Link to='/'>Home</Link>
+							</li>
+							<li>
+								<Link to='/update-profile'>update</Link>
+							</li>
+							<li>
+								<Link to='/signup'>signup</Link>
+							</li>
+							<li>
+								<Link to='/login'>login</Link>
+							</li>
+							<li>
+								<Link to='/forgot-password'>forgot password</Link>
+							</li>
+						</ul>
+					</Card.Body>
+				</Card>
+			)}
+			<div className='w-50' styles={{ maxWidth: '400px' }}>
+				<Switch>
+					<PrivateRoute exact path='/' component={Dashboard} />
+					<PrivateRoute path='/update-profile' component={UpdateProfile} />
+					<Route path='/signup' component={Signup} />
+					<Route path='/login' component={Login} />
+					<Route path='/forgot-password' component={ForgotPassword} />
+				</Switch>
+			</div>
+		</Container>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
