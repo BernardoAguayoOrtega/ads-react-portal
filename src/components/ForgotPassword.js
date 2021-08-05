98b1e77f580537c08b21764280b6cdf9f95a7b0d
Ricardo Ramirez <ricardo.rmeza@live.com>
2021-08-04 21:30:38 -0500

    add forgor password success

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,29 +1,28 @@
 import React, { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export default function  Login() {
-
+export default function  ForgotPassword() {
     const emailRef = useRef();
-    const passwordRef = useRef();
-    // const passwordConfirmRef = useRef();
-    const { login } = useAuth();
+    const { resetPassword } = useAuth();
     const [ error, setError ] = useState("");
+    const [ message, setMessage ] = useState("");
     const [ loading, setLoading ] = useState(false);
-    const history = useHistory();
+    
 
     async function handleSubmit(e) {
         e.preventDefault();
     
         
         try {
-          setError("");
-          setLoading(true);
-          await login(emailRef.current.value, passwordRef.current.value);  
-          history.push("/")
+            setMessage('');
+            setError("");
+            setLoading(true);
+            await resetPassword(emailRef.current.value)
+            setMessage('Revisa tu correo electrónico para instrucciones');
         } catch {
-          setError("Oops! No se puede iniciar sesión");
+          setError("Oops! No se puede generar nueva contraseña");
         }
     
         setLoading(false);
@@ -33,7 +32,7 @@ export default function  Login() {
         <>
         <Card>
             <Card.Body>
-                <h2 className="text-center mb-4">Inicia Sesión</h2>
+                <h2 className="text-center mb-4">Reset Contraseña</h2>
                 {/* { currentUser.email } */}
                 {error && <Alert variant="danger">{error}</Alert>}
                 <Form onSubmit={handleSubmit}>
@@ -41,16 +40,12 @@ export default function  Login() {
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email" ref={emailRef} required />
                     </Form.Group>
-                    <Form.Group id="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" ref={passwordRef} required />
-                    </Form.Group>
                     <Button disabled={loading} className="w-100" type='submit'>
-                        Iniciar
+                        Generar Nueva Contraseña
                     </Button>
                 </Form>
                 <div className="w-100 text-center mt-3">
-                    <Link to="/forgot-password">Olvide mi contraseña</Link>
+                    <Link to="/login">Iniciar Sesión</Link>
                 </div>    
             </Card.Body>
         </Card>
