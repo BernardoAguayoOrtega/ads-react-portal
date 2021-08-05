0c8d4af07882b3aff39262c4da130d1c764b5997
Ricardo Ramirez <ricardo.rmeza@live.com>
2021-08-04 21:49:38 -0500

    change lenguage

--- src/components/Login.js
+++ src/components/Login.js
@@ -38,11 +38,11 @@ export default function  Login() {
                 {error && <Alert variant="danger">{error}</Alert>}
                 <Form onSubmit={handleSubmit}>
                     <Form.Group id="email">
-                        <Form.Label>Email</Form.Label>
+                        <Form.Label>Correo Electrónico</Form.Label>
                         <Form.Control type="email" ref={emailRef} required />
                     </Form.Group>
                     <Form.Group id="password">
-                        <Form.Label>Password</Form.Label>
+                        <Form.Label>Contraseña</Form.Label>
                         <Form.Control type="password" ref={passwordRef} required />
                     </Form.Group>
                     <Button disabled={loading} className="w-100" type='submit'>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -40,18 +40,18 @@ export default function  Signup() {
                 {error && <Alert variant="danger">{error}</Alert>}
                 <Form onSubmit={handleSubmit}>
                     <Form.Group id="email">
-                        <Form.Label>Email</Form.Label>
+                        <Form.Label>Correo Electrónico</Form.Label>
                         <Form.Control type="email" ref={emailRef} required />
                     </Form.Group>
                     <Form.Group id="password">
-                        <Form.Label>Password</Form.Label>
+                        <Form.Label>Contraseña</Form.Label>
                         <Form.Control type="password" ref={passwordRef} required />
                     </Form.Group>
                     <Form.Group id="password-confirm">
-                        <Form.Label>Password Confirm</Form.Label>
+                        <Form.Label>Confirma Contraseña</Form.Label>
                         <Form.Control type="password" ref={passwordConfirmRef} required />
                     </Form.Group>
-                    <Button disabled={loading} className="w-100" type='submit'>
+                    <Button disabled={loading} className="w-100 mt-1" type='submit'>
                         Crear cuenta
                     </Button>
                 </Form>
